db905e1dea06085daff40a674cb8c93a99efa918
Jose Bautista <jose.bautista@alumnos.ucn.cl>
2024-11-17 23:11:45 -0300

    (feat) CRUD Initial

---
+++ Pages/Actualizar/ActualizarBus.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ActualizarBusModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
