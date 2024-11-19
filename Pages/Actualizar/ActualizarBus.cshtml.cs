4a866d5feaff229a67a2814db7482779267febd1
Jose Bautista <jose.bautista@alumnos.ucn.cl>
2024-11-18 21:57:54 -0300

    (feat) added Actualizar all models.

--- Pages/Actualizar/ActualizarBus.cshtml.cs
+++ Pages/Actualizar/ActualizarBus.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Transportes_Mardones_Torres;
 
 namespace MyApp.Namespace
 {
     public class ActualizarBusModel : PageModel
     {
-        public void OnGet()
+        public Bus bus { get; set; }
+        public IActionResult OnGet(int? id)
         {
+            TransporteContext context = new TransporteContext();
+            // Aquí se recibe el id del bus a actualizar
+            if(!id.HasValue)
+            {
+                // Si no se recibe el id, se redirige a la página de inicio
+                Response.Redirect("/");
+            }
+            bus = context.Buses.Find(id);
+            
+            return Page();
+
+        }
+        public IActionResult OnPost(Bus bus)
+        {
+            TransporteContext context = new TransporteContext();
+            // Se actualiza el bus
+            context.Buses.Update(bus);
+            context.SaveChanges();
+            // Se redirige a la página de inicio
+            return Redirect("/");
         }
     }
 }
--- Pages/Actualizar/ActualizarChofer.cshtml.cs
+++ Pages/Actualizar/ActualizarChofer.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Transportes_Mardones_Torres;
 
 namespace MyApp.Namespace
 {
     public class ActualizarChoferModel : PageModel
     {
+        public Chofer chofer { get; set; }
         public void OnGet()
         {
+            TransporteContext context = new TransporteContext();
+            // Aquí se recibe el id del chofer a actualizar
+            if(!Request.Query.ContainsKey("id"))
+            {
+                // Si no se recibe el id, se redirige a la página de inicio
+                Response.Redirect("/");
+            }
+            chofer = context.Choferes.Find(int.Parse(Request.Query["id"]));
+
+
+        }
+        public IActionResult OnPost(Chofer chofer)
+        {
+            TransporteContext context = new TransporteContext();
+            // Se actualiza el chofer
+            context.Choferes.Update(chofer);
+            context.SaveChanges();
+            // Se redirige a la página de inicio
+            return Redirect("/");
         }
     }
 }
