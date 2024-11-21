38f70deb505b8e508f0e256e1877a3c1d6eac273
Ertinho <105566974+Ertinho@users.noreply.github.com>
2024-11-21 01:55:27 -0300

    feat(Pages): create a new Chofer
    - add a dropdown in navigation bar for Choferes, Buses, Viajes.

--- Pages/Crear/NuevoChofer.cshtml.cs
+++ Pages/Crear/NuevoChofer.cshtml.cs
@@ -1,12 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Transportes_Mardones_Torres;
 namespace MyApp.Namespace
 {
     public class NuevoChoferModel : PageModel
     {
         public void OnGet()
         {
+        }
+
+        public void OnPost(string nombre, string apellido, bool disponibilidad)
+        {
+            Chofer chofer = new Chofer();
+            chofer.Nombre = nombre;
+            chofer.Apellido = apellido;
+            chofer.Disponibilidad = disponibilidad;
+            
+            TransporteContext context = new TransporteContext();
+            context.Choferes.Add(chofer);
+            context.SaveChanges();
+
+            Response.Redirect("/Listar/ListarChoferes");
         }
     }
 }
