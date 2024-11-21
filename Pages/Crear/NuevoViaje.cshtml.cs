05356efcf0ff6d9758abd177e5b4ad2822174b01
Ertinho <105566974+Ertinho@users.noreply.github.com>
2024-11-21 02:58:23 -0300

    feat(Pages): create a new Viaje
    -add validation in the forms

--- Pages/Crear/NuevoViaje.cshtml.cs
+++ Pages/Crear/NuevoViaje.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Transportes_Mardones_Torres;
 namespace MyApp.Namespace
 {
     public class NuevoViajeModel : PageModel
     {
+        public List<Bus> ListaBuses { get; set; }
+        public List<Chofer> ListaChoferes { get; set; }
+
         public void OnGet()
         {
+            TransporteContext context = new TransporteContext();
+            ListaBuses = context.Buses.Where(bus => bus.Disponibilidad).ToList();
+            ListaChoferes = context.Choferes.Where(chofer => chofer.Disponibilidad).ToList();
+        }
+
+        public void OnPost(int bus, int chofer, DateTime fecha, string origen, string destino)
+        {
+            Viaje viaje = new Viaje();
+            viaje.IdBus = bus;
+            viaje.IdChofer = chofer;
+            viaje.FechaInicio = fecha;
+            viaje.CiudadOrigen = origen;
+            viaje.CiudadDestino = destino;
+            viaje.Estado = "Pendiente";
+
+            
+            //TODO: Falta hacer la tabla de kilometraje y ¿calcular? la distancia
+
+
+
+            TransporteContext context = new TransporteContext();
+            context.Viajes.Add(viaje);
+            context.SaveChanges();
+
+            Response.Redirect("/Listar/ListarViajes");
         }
     }
 }
